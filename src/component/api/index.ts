4bdf7934e86024e9ec86cde2f8fe5cce09f30d06
abupraise <praiseabu2000@gmail.com>
2024-10-17 20:07:03 +0100

    Updated -

--- src/component/api/index.ts
+++ src/component/api/index.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response } from 'express';
 import { MongoClient, ObjectId } from 'mongodb';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+}));
 
 let db: any;
 
