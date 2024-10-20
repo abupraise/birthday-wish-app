f26fb34e1cee30e0e47ecbf265a1d833bdabb97b
abupraise <praiseabu2000@gmail.com>
2024-10-20 22:11:07 +0100

    Latest Updated Code

--- src/component/utils.ts
+++ src/component/utils.ts
@@ -1,5 +1,5 @@
 export function generateUniqueId(celebrantName: string): string {
-  const namePart = celebrantName.toLowerCase().replace(/\s+/g, '').substr(0, 6);
-  const randomPart = Math.random().toString(36).substr(2, 4);
-  return `${namePart}-${randomPart}`;
+  const namePart = celebrantName.split(' ')[0].toLowerCase();
+  return namePart;
 }
+
